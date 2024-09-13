1617ddf76fd64777fa6b21dce53db292d2495a82
Henry Centurion <u20221a339@upc.edu.pe>
2024-09-13 16:35:20 -0500

    feat: add landing contact and footer section

--- landing-page/js/menu.js
+++ landing-page/js/menu.js
@@ -2,13 +2,22 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const menuItems = document.querySelectorAll('.nav__link a');
 
-    openButton.addEventListener('click', ()=>{
+    
+    openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
     });
 
-    closeMenu.addEventListener('click', ()=>{
+    
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
 
+    
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show');
+        });
+    });
 })();
